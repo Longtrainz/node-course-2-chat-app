2a11740716c3c214a829fe4039bf7e048ee17bbb
Vl Bu <chinazaro@gmail.com>
2018-12-10 14:04:01 +0200

    Format timestamps using moment js

--- public/js/index.js
+++ public/js/index.js
@@ -9,25 +9,27 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function (message) {
-    console.log('New message', message);  
+    let formattedTime = moment(message.createdAt).format('h:mm a');
 
     let li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+    li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
     jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
+    let formattedTime = moment(message.createdAt).format('h:mm a');
     let li = jQuery('<li></li>');
     let a = jQuery('<a target="_blank">My current location</a>');
 
-    li.text(`${message.from}: `);
+    li.text(`${message.from} ${formattedTime}: `);
     a.attr('href', message.url);
     li.append(a);
 
     jQuery('#messages').append(li);
 });
 
+// Create message, listen to submit button
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
 
@@ -41,7 +43,7 @@ jQuery('#message-form').on('submit', function (e) {
     });
 });
 
-// Get location by clicking button
+// Crete location message, Get location by clicking button
 let locationButton = jQuery('#send-location');
 locationButton.on('click', () => {
     if (!navigator.geolocation) {
